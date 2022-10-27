0af9b58cdc6e1706ed76544de33441f89f67ada1
Vlad Shmaliukh <zskat02@gmail.com>
2022-10-27 19:25:43 +0300

    Hot fix

--- src/main/java/com/nonamed/hackathon2022testgame/MainMenuController.java
+++ src/main/java/com/nonamed/hackathon2022testgame/MainMenuController.java
@@ -3,11 +3,16 @@ package com.nonamed.hackathon2022testgame;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainMenuController implements Initializable {
+
+    @FXML
+    protected Button exitButton;
 
     @FXML
     protected void onNewGameButtonClick() {
@@ -20,6 +25,12 @@ public class MainMenuController implements Initializable {
         App.stage.setScene(new SettingsMenu().settingsMenuScene);
     }
 
+    @FXML
+    protected void onCloseButtonClick(){
+        Stage stage = (Stage) exitButton.getScene().getWindow();
+        // do what you have to do
+        stage.close();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
